eb963632a9c01bb595d2de9a5f14d5abd04e830c
michal-hynek <hynek.michal@gmail.com>
2025-06-05 05:09:04 -0700

    Reverse String

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,20 +1,5 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
-    if input.is_empty() {
-        return input.to_string();
-    }
-
-    let chars = input.as_bytes();
-    let mut reversed_chars: Vec<u8> = vec![0; input.len()];
-    let mut left = 0;
-    let mut right = input.len()-1;
-
-    while left < right {
-        reversed_chars[left] = chars[right];
-        reversed_chars[right] = chars[left];
-
-        left += 1;
-        right -=1;
-    }
-
-    String::from_utf8_lossy(chars).to_string()
+    input.graphemes(true).rev().collect()
 }
