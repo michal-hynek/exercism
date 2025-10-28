c971063658b148d1cc87f85ab30dcd4cb1745c7d
michal-hynek <hynek.michal@gmail.com>
2025-10-28 06:24:29 -0700

    C++ - Gigasecond

---
+++ cpp/gigasecond/gigasecond.cpp
@@ -0,0 +1,11 @@
+#include "gigasecond.h"
+
+using namespace boost::posix_time;
+
+namespace gigasecond {
+
+    ptime advance(ptime time) {
+        return time + seconds(1000000000);
+    }
+
+}  // namespace gigasecond
---
+++ cpp/gigasecond/gigasecond.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "boost/date_time/posix_time/posix_time.hpp"
+
+namespace gigasecond {
+
+    boost::posix_time::ptime advance(boost::posix_time::ptime time);
+
+}  // namespace gigasecond
