530bf0177a16c5943d57c95dd4e140605163cb79
michal-hynek <hynek.michal@gmail.com>
2026-01-05 08:51:48 -0800

    C - Binary Search

---
+++ c/binary-search/binary_search.c
@@ -0,0 +1,20 @@
+#include "binary_search.h"
+
+const int *binary_search(int value, const int *arr, size_t length) {
+    int left = 0;
+    int right = length-1;
+
+    while (left <= right) {
+        int middle = left + (right - left) / 2;
+
+        if (arr[middle] == value) {
+            return &arr[middle];
+        } else if (arr[middle] < value) {
+            left = middle+1;
+        } else {
+            right = middle-1;
+        }
+    }
+
+    return NULL;
+}
