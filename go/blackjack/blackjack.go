5add9f51fb9f486835c326638363724df5109a55
michal-hynek <hynek.michal@gmail.com>
2026-01-22 07:38:21 -0800

    Go - Blackjack

---
+++ go/blackjack/blackjack.go
@@ -0,0 +1,61 @@
+package blackjack
+
+// ParseCard returns the integer value of a card following blackjack ruleset.
+func ParseCard(card string) int {
+	switch card {
+        case "ace":
+    		return 11
+    	case "king":
+    		return 10
+        case "queen":
+    		return 10
+        case "jack":
+    		return 10
+        case "ten":
+    		return 10
+        case "nine":
+    		return 9
+        case "eight":
+    		return 8
+        case "seven":
+    		return 7
+        case "six":
+    		return 6
+        case "five":
+    		return 5
+        case "four":
+    		return 4
+        case "three":
+    		return 3
+        case "two":
+    		return 2
+        default:
+    		return 0
+    }
+}
+
+// FirstTurn returns the decision for the first turn, given two cards of the
+// player and one card of the dealer.
+func FirstTurn(card1, card2, dealerCard string) string {
+    handValue := ParseCard(card1) + ParseCard(card2)
+    dealerValue := ParseCard(dealerCard)
+    
+	switch {
+        case card1 == "ace" && card2 == "ace":
+    		return "P"
+        case handValue == 21 && dealerValue < 10:
+    		return "W"
+         case handValue == 21 && dealerValue >= 10:
+    		return "S"
+        case handValue >= 17 && handValue <= 20:
+    		return "S"
+        case handValue >= 12 && handValue <= 16 && dealerValue < 7:
+    		return "S"
+        case handValue >= 12 && handValue <= 16 && dealerValue >= 7:
+    		return "H"
+        case handValue <= 11:
+    		return "H"
+        default:
+    		return "invalid action"
+    }
+}
