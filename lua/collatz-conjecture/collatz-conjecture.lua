d48e32c6e7ab07d10497eacec03220c761a8304b
michal-hynek <hynek.michal@gmail.com>
2026-02-11 07:30:36 -0800

    Lua - Collatz Conjecture

---
+++ lua/collatz-conjecture/collatz-conjecture.lua
@@ -0,0 +1,19 @@
+return function(n)
+    if n < 1 then
+        return error("n must be a positive number")
+    end
+
+    local steps = 0
+
+    while n > 1 do
+        if n % 2 == 0 then
+            n = n / 2
+        else
+            n = 3*n + 1
+        end
+
+        steps = steps + 1
+    end
+
+    return steps
+end
